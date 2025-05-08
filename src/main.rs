cc30ce5d2d318797636eb6fb083458c3c37b820a
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-05-08 17:37:04 +0200

    :hammer: Updated vectors
    * The vectors are now consequtive floats to make a predictible output for the unit-tests (on its way.)

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     // construct vector
     // NOTE: the vec!-macro is a must. That is just
     // how it works. Otherwise its an array.
-    let vector_numeric: Vec<f64> = vec![1.2, 1.3, 1.9, 1.2, 99.1];
+    let vector_numeric: Vec<f64> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
 
     // call function
     simple_moving_average(vector_numeric, 2);
@@ -19,7 +19,7 @@ fn main() {
     // Error: error[E0382]: use of moved value: `vector_numeric`
     // For more information about this error, try `rustc --explain E0382`.
     // For later studies
-    let new_vector: Vec<f64> = vec![1.2, 1.3, 1.9, 1.2, 99.1];
+    let new_vector: Vec<f64> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
 
     let container = sma_container {
         x: new_vector, // might be illegal to reuse. Lets see.
