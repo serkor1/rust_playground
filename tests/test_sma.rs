36b6acdeccafa39dcb09057ef2b9318f5acdf6e5
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-05-08 18:15:12 +0200

    :hammer: added unit-tests
    * The unit-tests works a bit like {testthat}, easy enough.

---
+++ tests/test_sma.rs
@@ -0,0 +1,13 @@
+use rust_playground::simple_approach::sma::{self, simple_moving_average};
+
+#[test]
+fn it_return_correct_values() {
+    // target value
+    let target_value: Vec<f64> = vec![1.5, 2.5, 3.5, 4.5, 5.5, 0.0];
+
+    // input value
+    let input_value: Vec<f64> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
+    let result: Vec<f64> = simple_moving_average(input_value, 2);
+
+    assert_eq!(result, target_value);
+}
